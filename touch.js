8b8763c378b654abbeb799489b478b9e85bf58ba
Jethro-Rabbi Marcial Villena <xeqtioner10@gmail.com>
2023-06-25 04:08:21 +0800

    change visor ang fix responsiveness

--- touch.js
+++ touch.js
@@ -1,36 +1,3 @@
-// touchElement(document.querySelector(".navbar-toggler"));
-    
-// function touchElement(elmnt) {
-//     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    
-//     elmnt.ontouchstart = touchTouchStart(e);
-    
-//     function touchTouchStart(e) {
-//       e.preventDefault();
-//       pos3 = e.clientX;
-//       pos4 = e.clientY;
-//       document.ontouchend = closeTouchElement;
-//       document.ontouchmove = elementTouch;
-//     }
-  
-//     function elementTouch(e) {
-//       e = e || window.event;
-//       e.preventDefault();
-//       pos1 = pos3 - e.clientX;
-//       pos2 = pos4 - e.clientY;
-//       pos3 = e.clientX;
-//       pos4 = e.clientY;
-//       elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-//       elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-//     }
-  
-//     function closeTouchElement() {
-//       document.ontouchend = null;
-//       document.ontouchmove = null;
-//     }
-//   }
-
-
 window.onload = function()  {
 
 var touchmode   = document.querySelector(".navbar-toggler");
@@ -51,7 +18,5 @@ touchmode.addEventListener('touchend', function(ev){
     var y = parseInt(touchmode.style.top);
 
 })
-
-
 
 }
