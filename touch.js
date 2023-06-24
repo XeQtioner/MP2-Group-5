909de333d420a27019a9333a91ca18918f5b9507
Jethro-Rabbi Marcial Villena <xeqtioner10@gmail.com>
2023-06-24 17:29:03 +0800

    touch progress

---
+++ touch.js
@@ -0,0 +1,56 @@
+// touchElement(document.querySelector(".navbar-toggler"));
+    
+// function touchElement(elmnt) {
+//     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    
+//     elmnt.ontouchstart = touchTouchStart(e);
+    
+//     function touchTouchStart(e) {
+//       e.preventDefault();
+//       pos3 = e.clientX;
+//       pos4 = e.clientY;
+//       document.ontouchend = closeTouchElement;
+//       document.ontouchmove = elementTouch;
+//     }
+  
+//     function elementTouch(e) {
+//       e = e || window.event;
+//       e.preventDefault();
+//       pos1 = pos3 - e.clientX;
+//       pos2 = pos4 - e.clientY;
+//       pos3 = e.clientX;
+//       pos4 = e.clientY;
+//       elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+//       elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+//     }
+  
+//     function closeTouchElement() {
+//       document.ontouchend = null;
+//       document.ontouchmove = null;
+//     }
+//   }
+
+
+window.onload = function()  {
+
+var touchmode   = document.querySelector(".navbar-toggler");
+
+touchmode.addEventListener('touchmove', function(ev){
+    
+    var touchLocation = ev.targetTouches[0];
+
+    touchmode.style.left = touchLocation.pageX + 'px';
+    touchmode.style.top = touchLocation.pageY + 'px';
+
+})
+
+touchmode.addEventListener('touchend', function(ev){
+
+    var x = parseInt(touchmode.style.left);
+    var y = parseInt(touchmode.style.top);
+
+})
+
+
+
+}
