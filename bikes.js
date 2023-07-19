e8458c519795fac89ecfe251d1147711c649cc5c
Jethro-Rabbi Marcial Villena <xeqtioner10@gmail.com>
2023-07-19 18:17:46 +0800

    snap scroll

--- bikes.js
+++ bikes.js
@@ -278,15 +278,18 @@ function resetCardValues() {
 }
 
 function enterFullscreen(element) {
-  if (element.requestFullscreen) {
-    element.requestFullscreen();
-  } else if (element.mozRequestFullScreen) { // Firefox
-    element.mozRequestFullScreen();
-  } else if (element.webkitRequestFullscreen) { // Chrome, Safari and Opera
-    element.webkitRequestFullscreen();
-  } else if (element.msRequestFullscreen) { // Internet Explorer/Edge
-    element.msRequestFullscreen();
-  }
+	if (element.requestFullscreen) {
+		element.requestFullscreen();
+	} else if (element.mozRequestFullScreen) {
+		// Firefox
+		element.mozRequestFullScreen();
+	} else if (element.webkitRequestFullscreen) {
+		// Chrome, Safari and Opera
+		element.webkitRequestFullscreen();
+	} else if (element.msRequestFullscreen) {
+		// Internet Explorer/Edge
+		element.msRequestFullscreen();
+	}
 }
 
 // Example usage
